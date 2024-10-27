259c0c405e114cbae66c8520944462cf45236c3e
gusCreator <lgsc21211@gmail.com>
2024-10-26 22:15:44 -0500

    Se configura el routing para webpack

--- Unity/Exoplanets/react/webpack.config.js
+++ Unity/Exoplanets/react/webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = (env, originalConfig) => originalConfig;
+const path = require('path');
+
+module.exports = (env, originalConfig) => ({
+  ...originalConfig,
+  resolve: {
+    ...originalConfig.resolve,
+    alias: {
+      ...(originalConfig.resolve.alias || {}),
+      '@i18n': path.resolve(__dirname, 'src/i18n'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@mytypes': path.resolve(__dirname, 'src/types'),
+    },
+  },
+});
