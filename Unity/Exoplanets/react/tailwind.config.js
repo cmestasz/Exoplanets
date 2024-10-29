f932167f4ed6e5223ea1f4d99ab3f51ee2f42eda
gusCreator <lgsc21211@gmail.com>
2024-10-29 17:54:36 -0500

    Añadiendo utilidades de tailwind compatibles con ReactUnity

--- Unity/Exoplanets/react/tailwind.config.js
+++ Unity/Exoplanets/react/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './src/components/**/*.{js,jsx,ts,tsx}',
@@ -5,6 +7,24 @@ module.exports = {
     './src/app/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    translate: {
+      0: '0',
+      1: '1',
+      2: '2',
+      4: '4',
+      8: '8',
+      10: '10',
+    },
+    stateDuration: {
+      0: '0s',
+      100: '0.1s',
+      300: '0.3s',
+      500: '0.5s',
+      700: '0.7s',
+      800: '0.8s',
+      900: '0.9s',
+      1000: '1s',
+    },
     extend: {
       colors: {
         primary: 'var(--primary)',
@@ -30,5 +50,39 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'trans-x': (value) => ({
+            '--trans-val-x': `${value}px`,
+            translate: 'var(--trans-val-x) var(--trans-val-y)',
+          }),
+          'trans-y': (value) => ({
+            '--trans-val-y': `${value}px`,
+            translate: 'var(--trans-val-x) var(--trans-val-y)',
+          }),
+          trans: (value) => ({
+            '--trans-val-x': `${value}px`,
+            '--trans-val-y': `${value}px`,
+            translate: 'var(--trans-val-x) var(--trans-val-y)',
+          }),
+        },
+        {
+          values: theme('translate'),
+          supportsNegativeValues: true,
+        },
+      );
+      matchUtilities(
+        {
+          'state-duration': (value) => ({
+            'state-duration': value,
+          }),
+        },
+        {
+          values: theme('stateDuration'),
+        },
+      );
+    }),
+  ],
 };
