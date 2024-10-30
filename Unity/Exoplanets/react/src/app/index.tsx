ef4cc2810eb03b6968b7f7394828b010db59c521
gusCreator <lgsc21211@gmail.com>
2024-10-30 17:43:03 -0500

    Se utiliza un contexto para lanzar las alertas desde los componentes

--- Unity/Exoplanets/react/src/app/index.tsx
+++ Unity/Exoplanets/react/src/app/index.tsx
@@ -4,22 +4,43 @@ import '@i18n/i18n';
 import { useTranslation } from 'react-i18next';
 import AstrosSlider from '@components/astros/AstrosSlider';
 import { kepler22b, proximaCentauriB } from '@lib/mock';
+import Input from '@components/form/input/Input';
+import { useAlert } from '@lib/hooks';
+import Alert, { AlertContext } from '@components/alerts/Alert';
 
 function App() {
   const { t } = useTranslation();
+  const {
+    alertOptions, hideAlert, showAlert, isVisible,
+  } = useAlert();
   const handleClick = () => {
     console.log('An Astro card');
   };
   return (
-    <div className="flex">
-      <h1 className="text-white">{t('components.form.input.error-update')}</h1>
-      <h2 className="text-white">ods</h2>
-      <AstrosSlider
-        astros={[
-          { onClick: handleClick, astro: kepler22b },
-          { onClick: handleClick, astro: proximaCentauriB },
-        ]}
-      />
+    <div className="flex relative">
+      <AlertContext.Provider value={showAlert}>
+        <h1 className="text-white">{t('components.form.input.error-update')}</h1>
+        <h2 className="text-white">ods</h2>
+        <AstrosSlider
+          astros={[
+            { onClick: handleClick, astro: kepler22b },
+            { onClick: handleClick, astro: proximaCentauriB },
+          ]}
+        />
+        <Input
+          name={t('components.user.login')}
+          label="other label"
+          defaultValue="xd"
+        />
+        {
+          isVisible && (
+            <Alert
+              alertOptions={alertOptions}
+              hideAlert={hideAlert}
+            />
+          )
+        }
+      </AlertContext.Provider>
     </div>
   );
 }
--- Unity/Exoplanets/react/src/components/alerts/Alert.tsx
+++ Unity/Exoplanets/react/src/components/alerts/Alert.tsx
@@ -1,44 +1,39 @@
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { FaXmark } from 'react-icons/fa6';
-import { InOutStates } from '@lib/hooks';
+import { createContext } from 'react';
 import { AlertOptions } from './types';
+
+export const AlertContext = createContext<({ message, duration, type }: AlertOptions) => void
+>(null);
 
 export interface AlertProps {
   alertOptions: AlertOptions;
-  state: InOutStates;
   hideAlert: () => void;
 }
 
 export default function Alert({
-  alertOptions, state, hideAlert,
+  alertOptions, hideAlert,
 }: AlertProps) {
-  const baseStyle = 'fixed top-0 right-0 rounded-md border-2 max-w-56 w-fit p-3 shadow-lg z-50 flex gap-2 m-1.5 text-sm font-exo';
+  const baseStyle = 'absolute top-0 right-0 rounded-md border-2 max-w-56 w-fit p-3 shadow-lg z-50 flex flex-row gap-2 m-1.5 text-sm font-exo transition-all enter:-trans-y-[120%] enter:opacity-0 enter:state-duration-0 state-duration-800 leave:-trans-y-[120%]';
   const determinateStyle = clsx({
     'bg-green-dark text-green border-green': alertOptions.type === 'success',
     'bg-red-dark text-red border-red': alertOptions.type === 'error',
   });
 
   return (
-    state !== 'initial' && (
-      <div
-        key="alert"
-        className={twMerge(
-          baseStyle,
-          determinateStyle,
-          clsx({
-            'animate-alert-in': state === 'in',
-            'animate-alert-out': state === 'out',
-          }),
-        )}
-      >
-        <span>{alertOptions.message}</span>
-        <FaXmark
-          onClick={hideAlert}
-          className="cursor-pointer"
-        />
-      </div>
-    )
-
+    <view
+      className={twMerge(
+        baseStyle,
+        determinateStyle,
+      )}
+      style={{ motion: '0.7s 0s ease-in-out' }}
+    >
+      <span className="flex-auto">{alertOptions.message}</span>
+      <FaXmark
+        onClick={hideAlert}
+        className="cursor-pointer text-red flex-auto"
+      />
+    </view>
   );
 }
