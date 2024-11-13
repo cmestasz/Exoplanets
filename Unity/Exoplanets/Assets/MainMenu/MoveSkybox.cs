036c2d9a74b894dd53c519e9c6177b85d42bd569
gusCreator <lgsc21211@gmail.com>
2024-11-13 16:48:42 -0500

    Script for background movement has been updated. Now, the background moves from left to right, then to down, after to right and returns on the original position

--- Unity/Exoplanets/Assets/MainMenu/MoveSkybox.cs
+++ Unity/Exoplanets/Assets/MainMenu/MoveSkybox.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class MoveSkybox : MonoBehaviour
 {
-    public float speed = 0.1f;
-    
+    public float speed = 0.5f;
+    private Vector3 direction = Vector3.right;
+
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+
+        if (Time.time % 4 < 1)
+        {
+            direction = Vector3.right;
+        }
+        else if (Time.time % 4 < 2)
+        {
+            direction = Vector3.down;
+        }
+        else if (Time.time % 4 < 3)
+        {
+            direction = Vector3.left;
+        }
+        else
+        {
+            direction = Vector3.up;
+        }
+
+        RenderSettings.skybox.SetVector("_MainTex_ST", new Vector4(direction.x * Time.time * speed, direction.y * Time.time * speed, 0, 0));
     }
 }
