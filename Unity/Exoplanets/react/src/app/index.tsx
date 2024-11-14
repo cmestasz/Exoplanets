bfad8f33fdc6ac21f8a6cd9ca936ac56f707a513
Gocardi <diegoalejandrocarbajal@gmail.com>
2024-11-13 21:20:00 -0500

    error en la implementacion de router

--- Unity/Exoplanets/react/src/app/index.tsx
+++ Unity/Exoplanets/react/src/app/index.tsx
@@ -6,6 +6,8 @@ import { useTranslation } from 'react-i18next';
 import AstrosSlider from '@components/astros/AstrosSlider';
 import { kepler22b, proximaCentauriB } from '@lib/mock';
 import Input from '@components/form/input/Input';
+import { useAlert } from '@lib/hooks';
+import Alert, { AlertContext } from '@components/alerts/Alert';
 import SelectLanguage from '@components/languages/SelectLanguage';
 import { useMemo } from 'react';
 import UserAuth from '@components/user/UserAuth';
@@ -19,36 +21,50 @@ import CrearConstelacion from '@pages/CrearConstelacion';
 import MiCuenta from '@pages/MiCuenta';
 import Maximizado from '@pages/Maximizado';
 
-
 function App() {
-//  const { t } = useTranslation();
+  const { t } = useTranslation();
+  const {
+    alertOptions, hideAlert, showAlert, isVisible,
+  } = useAlert();
 
-//  const handleClick = () => {
-//    console.log('An Astro card');
-//  };
+  const handleClick = () => {
+    console.log('An Astro card');
+  };
 
-//  const astros = useMemo(
-//    () => [
-//      { onClick: handleClick, astro: kepler22b },
-//      { onClick: handleClick, astro: proximaCentauriB },
-//    ],
-//    []
-//  );
+  const astros = useMemo(() => [
+    { onClick: handleClick, astro: kepler22b },
+    { onClick: handleClick, astro: proximaCentauriB },
+  ], []);
 
   return (
     <BrowserRouter>
+      <AlertContext.Provider value={showAlert}>
+        <div>
+          <h1 className="text-white">{t('components.form.input.error-update')}</h1>
+          <SelectLanguage />
+          <UserAuth />
+          {isVisible && <Alert alertOptions={alertOptions} hideAlert={hideAlert} />}
 
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/exoplanetas" element={<Exoplanetas />} />
-          <Route path="/exoplanetas/:id" element={<VerExoplaneta />} />
-          <Route path="/estrellas" element={<Estrellas />} />
-          <Route path="/estrellas/:id" element={<VerEstrella />} />
-          <Route path="/estrellas/crear-constelacion" element={<CrearConstelacion />} />
-          <Route path="/mi-cuenta" element={<MiCuenta />} />
-          <Route path="/maximizado" element={<Maximizado />} />
-        </Routes>
-      
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/exoplanetas" element={<Exoplanetas />} />
+            <Route path="/exoplanetas/:id" element={<VerExoplaneta />} />
+            <Route path="/estrellas" element={<Estrellas />} />
+            <Route path="/estrellas/:id" element={<VerEstrella />} />
+            <Route path="/estrellas/crear-constelacion" element={<CrearConstelacion />} />
+            <Route path="/mi-cuenta" element={<MiCuenta />} />
+            <Route path="/maximizado" element={<Maximizado />} />
+          </Routes>
+
+          <h2 className="text-white">ods</h2>
+          <AstrosSlider astros={astros} />
+          <Input
+            name={t('components.user.login')}
+            label="other label"
+            defaultValue="xd"
+          />
+        </div>
+      </AlertContext.Provider>
     </BrowserRouter>
   );
 }
