761701eb02754734c280b77f9e4ada8a80180387
gusCreator <lgsc21211@gmail.com>
2024-11-21 22:38:50 -0500

    Setting placeholder and some style for search bar

--- Unity/Exoplanets/react/src/components/search/Search.tsx
+++ Unity/Exoplanets/react/src/components/search/Search.tsx
@@ -7,11 +7,22 @@ export default function Search({
 }: SearchProps) {
   return (
     <search
-      className="flex flex-row text-primary text-3xl border-2 border-primary rounded-lg px-4 py-2 gap-6"
+      className="flex flex-row text-primary text-3xl"
     >
-      <icon className="text-3xl">search</icon>
+      <button
+        className="border-2 border-primary px-4 hover:border-secondary hover:text-secondary transition-colors duration-300 ease-out rounded-none group m-duration-300 m-ease-out"
+        style={{ borderBottomLeftRadius: '0.5rem', borderTopLeftRadius: '0.5rem' }}
+      >
+        <icon
+          className="text-4xl group-active:scale-90"
+        >
+          search
+        </icon>
+
+      </button>
       <input
-        className="flex-auto w-full bg-transparent placeholder:text-quaternary text-secondary font-exo"
+        className="flex-grow bg-transparent placeholder:text-quaternary text-secondary font-exo border-2 border-primary px-4 py-2 rounded-none border-l-0"
+        style={{ borderBottomRightRadius: '0.5rem', borderTopRightRadius: '0.5rem' }}
         placeholder={placeholder}
       />
     </search>
