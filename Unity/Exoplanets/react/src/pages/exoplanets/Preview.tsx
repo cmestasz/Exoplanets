adeedd9fee0d34d307c08b00e6591472e30e8141
gusCreator <lgsc21211@gmail.com>
2024-11-23 15:07:33 -0500

    First version of exoplanets page

--- Unity/Exoplanets/react/src/pages/exoplanets/Preview.tsx
+++ Unity/Exoplanets/react/src/pages/exoplanets/Preview.tsx
@@ -1,4 +1,8 @@
+import { Text } from '@components/ui/Text';
 import { Exoplanet } from '@mytypes/astros';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
+import PreviewData from './PreviewData';
 
 interface PreviewProps {
   currentExoplanet: Exoplanet;
@@ -7,6 +11,8 @@ interface PreviewProps {
 export default function Preview({
   currentExoplanet,
 }: PreviewProps) {
+  const nav = useNavigate();
+  const { t } = useTranslation();
   if (!currentExoplanet) {
     return (
       <view className="size-7 animate-spin" />
@@ -14,17 +20,33 @@ export default function Preview({
   }
   return (
     <view
-      className="flex flex-col flex-1 p-6 gap-3 border-2 border-primary rounded-lg"
+      className="flex flex-col flex-1 p-6 gap-4 border-2 border-primary rounded-lg"
     >
       <img
         src={currentExoplanet.imageUrl}
         alt={currentExoplanet.name}
+        className="flex-auto"
       />
       <h3
         className="font-audiowide text-4xl text-center text-primary leading-10"
       >
         {currentExoplanet.name}
       </h3>
+      <hr className="border-primary border-[1px]" />
+      <Text
+        className="flex flex-row font-exo text-3xl text-primary"
+        asButton
+        onClick={() => nav(currentExoplanet.id)}
+      >
+        {t('pages.exoplanets.card.button')}
+        <icon className="text-2xl">open_in_new</icon>
+      </Text>
+      <hr className="border-primary border-[1px]" />
+      <PreviewData
+        radius={currentExoplanet.radius}
+        mass={currentExoplanet.mass}
+        disc_date={currentExoplanet.disc_date}
+      />
     </view>
   );
 }
