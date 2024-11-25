c40345e209d038d47fef6f70ed86342ed4c20969
gusCreator <lgsc21211@gmail.com>
2024-11-25 17:22:47 -0500

    Fixing login component

--- Unity/Exoplanets/react/src/components/user/Login.tsx
+++ Unity/Exoplanets/react/src/components/user/Login.tsx
@@ -14,22 +14,25 @@ export default function Login({
   const { t } = useTranslation();
   const userAction = useContext(UserContext);
   return (
-    <Text
-      asButton
-      invertedStyle
-      onClick={() => userAction.login()}
-      className={clsx('text-3xl gap-3 shrink', {
-        'px-3 py-10 max-h-32': !asideMenu,
-        'max-h-14 py-3': asideMenu,
-      })}
-    >
-      <img
-        src="/img/google.png"
-        alt="Google login"
-      />
-      <span className="overflow-clip whitespace-nowrap shrink">
-        {t('components.user.login')}
-      </span>
-    </Text>
+    <view className="flex">
+      <Text
+        asButton
+        invertedStyle
+        onClick={() => userAction.login()}
+        className={clsx('text-3xl gap-5', {
+          'px-3 py-10 max-h-32': !asideMenu,
+          'max-h-14 py-3': asideMenu,
+        })}
+      >
+        <img
+          src="/img/google.png"
+          alt="Google login"
+          className="flex-auto"
+        />
+        <span className="overflow-clip whitespace-nowrap shrink">
+          {t('components.user.login')}
+        </span>
+      </Text>
+    </view>
   );
 }
