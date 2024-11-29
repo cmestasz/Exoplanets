67f65d74eae381b7c8b4273b1580766e24ae77fb
gusCreator <lgsc21211@gmail.com>
2024-11-29 18:42:17 -0500

    Fixing colors and set some values

--- Unity/Exoplanets/react/src/styles/colors.ts
+++ Unity/Exoplanets/react/src/styles/colors.ts
@@ -1,5 +1,5 @@
 const transitionColor = 'transition-colors duration-300 ease-out';
-const DEFAULT_COLOR = `${transitionColor} text-primary hover:text-quaternary`;
+const DEFAULT_COLOR = `${transitionColor} text-primary hover:text-secondary`;
 const INVERTED_COLOR = `${transitionColor} text-secondary hover:text-primary`;
 
 export { DEFAULT_COLOR, INVERTED_COLOR };
