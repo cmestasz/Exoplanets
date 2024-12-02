f6bbe84de9ce7b92ea28ce9d3ce1e4db7596f35d
gusCreator <lgsc21211@gmail.com>
2024-12-02 18:12:25 -0500

    Get api url and api socket from environment

--- Unity/Exoplanets/react/src/config.ts
+++ Unity/Exoplanets/react/src/config.ts
@@ -1,2 +1,9 @@
-export const DATABASE_URL = Interop.System.Environment.GetEnvironmentVariable('DATABASE_URL') || '';
-export const DATABASE_KEY = Interop.System.Environment.GetEnvironmentVariable('DATABASE_KEY') || '';
+const DATABASE_URL = Interop.System.Environment.GetEnvironmentVariable('DATABASE_URL') || '';
+const DATABASE_KEY = Interop.System.Environment.GetEnvironmentVariable('DATABASE_KEY') || '';
+const API_DOMAIN = Interop.System.Environment.GetEnvironmentVariable('API_DOMAIN') || '';
+const API_URL = `http://${API_DOMAIN}`;
+const API_SOCKET = `ws://${API_DOMAIN}`;
+
+export {
+  DATABASE_KEY, DATABASE_URL, API_URL, API_SOCKET,
+};
